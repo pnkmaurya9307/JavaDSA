37e1a5b18b6d352078f9e83472b9faf0caaeaae1
Pankaj Mourya <pnkmaurya9307@gmail.com>
2026-07-05 15:01:29 +0530

    code 1

---
+++ LearnJava.java
@@ -0,0 +1,22 @@
+public class LearnJava {
+
+    public static int reverse(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int m = n % 10;
+            sum = (sum * 10) + m;
+            n = n / 10;     // ⭐ Missing statement
+        }
+
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        reverse(2465);
+
+        System.out.println("Start small. Ship something.");
+    }
+}
